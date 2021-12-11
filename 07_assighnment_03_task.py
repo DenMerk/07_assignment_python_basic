c5dd6b73bbad78c75f04e232cb2bc89efb6acf31
Denis Merkurev <merckuriev.denis@yandex.ru>
2021-12-11 18:31:09 +0300

    Added magic methods to compare students and lecturers

--- 07_assighnment_03_task.py
+++ 07_assighnment_03_task.py
@@ -15,6 +15,15 @@ class Student:
                f"Курсы в процессе обучения:\n" \
                f"Завершенные курсы:\n"
 
+    def _stu_average_grade(self, list_of_grades):
+        """
+        Метод для расчета средней оценки студета по предмету
+        """
+        self.st_average_grade = 0
+        if list_of_grades:
+            self.st_average_grade = sum(list_of_grades) / len(list_of_grades)
+        return self.st_average_grade
+
     def grades_for_lecturer(self, lecturer, course, grade):
         """
         Метод для оценки лекторов студентами
@@ -25,6 +34,30 @@ class Student:
                 lecturer.lecturer_grades[course] += [grade]
             else:
                 lecturer.lecturer_grades[course] = [grade]
+
+    def __lt__(self, other):
+        """
+        Перегрузка метода сравнения для определения лучшего студента
+        """
+        if isinstance(other, Student):
+            best_student = other.surname
+            if other._stu_average_grade(other.grades['Python']) < self._stu_average_grade(self.grades['Python']):
+                best_student = self.surname
+            return f"Студент {best_student} имеет лучшие оценки"
+        else:
+            print(f"Недопустимые данные")
+
+    def __gt__(self, other):
+        """
+        Перегрузка метода сравнения для определения лучшего студента (для второго оператора сравнения)
+        """
+        if isinstance(other, Student):
+            best_student = other.surname
+            if self._stu_average_grade(self.grades['Python']) > other._stu_average_grade(other.grades['Python']):
+                best_student = self.surname
+            return f"Студент {best_student} имеет лучшие оценки"
+        else:
+            print(f"Недопустимые данные")
 
 
 class Mentor:
@@ -51,14 +84,41 @@ class Lecturer(Mentor):
         """
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка: {self.average_grade}\n"
 
-    def lec_average_grade(self, list_of_grades):
+    def _lec_average_grade(self, list_of_grades):
         """
         Метод вычисляет среднюю оценку лектора за прочитанные лекции
         """
         if list_of_grades:
             self.average_grade = sum(list_of_grades) / len(list_of_grades)
+            return self.average_grade
         else:
             print(f"У лектора {self.name} {self.surname} нет оценок от студентов")
+
+    def __lt__(self, other):
+        """
+        Перегрузка метода сравнения для определению лектора с наилучшими оценками
+        """
+        if isinstance(other, Lecturer):
+            best_lecturer = self.surname
+            if self._lec_average_grade(self.lecturer_grades['Python']) < \
+                    other._lec_average_grade(other.lecturer_grades['Python']):
+                best_lecturer = other.surname
+            return f"Лектор {best_lecturer} имеет самые высокие оценки от студентов"
+        else:
+            print('Недопустимые данные')
+
+    def __gt__(self, other):
+        """
+        Перегрузка метода сравнения для определению лектора с наилучшими оценками (для второго оператора сравнения)
+        """
+        if isinstance(other, Lecturer):
+            best_lecturer = other.surname
+            if self._lec_average_grade(self.lecturer_grades['Python']) > \
+                    other._lec_average_grade(other.lecturer_grades['Python']):
+                best_lecturer = self.surname
+            return f"Лектор {best_lecturer} имеет самые высокие оценки от студентов"
+        else:
+            print('Недопустимые данные')
 
 
 class Reviewer(Mentor):
@@ -78,42 +138,6 @@ class Reviewer(Mentor):
         return f"Имя: {self.name}\nФамилия: {self.surname}\n"
 
 
-def compare_students(students, course):
-    """
-    Функция для сравнения студентов по их средним оценкам
-    """
-    best_student_grade = 0
-    best_student = None
-    for student in students:
-        if course in student.courses_in_progress and student.grades[course]:
-            student.st_average_grade = sum(student.grades[course]) / len(student.grades[course])
-        else:
-            print(f"Студент {student.name} {student.surname} не изучает {course} или у него нет оценок")
-        if student.st_average_grade > best_student_grade:
-            best_student = f"Лучший студент по {course} - {student.name} {student.surname}"
-            best_student_grade = student.st_average_grade
-    print(best_student)
-
-
-def compare_lecturers(lecturers, course):
-    """
-    Функция для сравнения лекторов по оценкам от студентов
-    :param lecturers:
-    :param course:
-    """
-    best_lecturer = None
-    best_lecturer_grade = 0
-    for lecturer in lecturers:
-        if course in lecturer.courses_attached:
-            lecturer.lec_average_grade(lecturer.lecturer_grades[course])
-        else:
-            print(f"Лектор {lecturer.name} {lecturer.surname} не читает курс по {course}")
-        if lecturer.average_grade > best_lecturer_grade:
-            best_lecturer = f"Лучший лектор по курсу {course} - {lecturer.name} {lecturer.surname}"
-            best_lecturer_grade = lecturer.average_grade
-    print(best_lecturer)
-
-
 student_list = []  # список студентов
 lecturer_list = []  # список лекторов
 
@@ -127,7 +151,7 @@ second_student.courses_in_progress += ['Python']
 python_reviewer = Reviewer('Python', 'Guru')
 python_reviewer.courses_attached += ['Python']
 python_reviewer.rate_hw(first_student, 'Python', 10)
-python_reviewer.rate_hw(first_student, 'Python', 3)
+# python_reviewer.rate_hw(first_student, 'Python', 3)
 python_reviewer.rate_hw(first_student, 'Python', 10)
 
 python_reviewer.rate_hw(second_student, 'Python', 9)
@@ -142,8 +166,7 @@ lecturer_list.append(second_lecturer)
 first_student.grades_for_lecturer(first_lecturer, 'Python', 9)
 first_student.grades_for_lecturer(second_lecturer, 'Python', 10)
 second_student.grades_for_lecturer(first_lecturer, 'Python', 10)
-second_student.grades_for_lecturer(second_lecturer, 'Python', 8)
+second_student.grades_for_lecturer(second_lecturer, 'Python', 10)
 
-compare_students(student_list, 'Python')
-compare_lecturers(lecturer_list, 'Python')
-
+print(first_student < second_student)
+print(first_lecturer > second_lecturer)
